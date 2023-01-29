faa64ae0339a29e22f65638e0408eeddc0518ca3
MichaelMDTrep <MichaelMDTrep@Gmail.com>
2023-01-29 01:59:35 -0500

    new try

--- imdb_clone/wsgi.py
+++ imdb_clone/wsgi.py
@@ -10,11 +10,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 import sys
 
-path = '/home/myusername/mysite'
+path = 'imdb_clone/home/myusername/'
 if path not in sys.path:
     sys.path.insert(0, path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'imdb_clone.settings'
 
 from django.core.wsgi import get_wsgi_application
 
